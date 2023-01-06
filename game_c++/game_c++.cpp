4b12629a482c79a6e5dc3d593a67250961e21f65
OSGbear <mihail.tkach.nik@gmail.com>
2023-01-07 01:04:04 +0200

    Добавьте файлы проекта.

---
+++ game_c++/game_c++.cpp
@@ -0,0 +1,36 @@
+﻿//Game Over 1.0
+
+/*#include <iostream>
+
+int main()
+{
+	std::cout << "Game over" << std::endl;
+	std::system("pause");
+	return 0;
+}*/
+
+//Geme Over 1.1 - демонстрация using
+
+/*#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Game over" << endl;
+	system("pause");
+	return 0;
+}*/
+
+//Geme Over 1.2 - демонстрация using
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Game over" << endl;
+	system("pause");
+
+	return 0;
+}
---
+++ game_stats0.1/game_stats0.1.cpp
@@ -0,0 +1,66 @@
+﻿//Game_stats_0.1
+#include <iostream>
+using namespace std;
+
+
+/*int main()
+{
+	int score;
+	double distance;
+	char playAgain;
+	bool shieldsUp;
+	short lives,aliensKilled;
+	score = 0;
+	distance = 1300.77;
+	playAgain = 'y';
+	shieldsUp = true;
+	lives = 5;
+	aliensKilled = 12;
+	double engineTemp = 6572.89;
+	cout << "\nscore: " << score << endl;
+	cout << "distance: " << distance << endl;
+	cout << "playAgain: " << playAgain << endl;
+	//shieldsUp пропустим, булевые значения не выводяться как правило.
+	cout << "lives: " << lives << endl;
+	cout << "aliensKilled: " << aliensKilled << endl;
+	cout << "engineTemp: " << engineTemp << endl;
+	int fuel;
+	cout << "\nHow much fuel? ";
+	cin >> fuel;
+	cout << "fuel: " << fuel << endl;
+	typedef unsigned short int ushort;
+	ushort bonus = 10;
+	cout << "\nbonus: " << bonus << endl;
+	return 0;
+}
+*/
+
+////Game_stats_0.11
+int main()
+{
+	int score=0;
+	double distance = 1300.77;
+	char playAgain = 'y';
+	bool shieldsUp = true;
+	short lives = 5, aliensKilled = 12;
+	double engineTemp = 6572.89;
+	cout << "\nscore: " << score << endl;
+	cout << "distance: " << distance << endl;
+	cout << "playAgain: " << playAgain << endl;
+	//shieldsUp пропустим, булевые значения не выводяться как правило.
+	cout << "lives: " << lives << endl;
+	cout << "aliensKilled: " << aliensKilled << endl;
+	cout << "engineTemp: " << engineTemp << endl;
+	int fuel;
+	cout << "\nHow much fuel? ";
+	cin >> fuel;
+	cout << "fuel: " << fuel << endl;
+	typedef unsigned short int ushort;
+	ushort bonus = 10;
+	cout << "\nbonus: " << bonus << endl;
+	return 0;
+}
+
+
+
+
