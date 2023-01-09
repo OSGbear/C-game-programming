118ba325af2b72029a211a1233a9f822dbf37cf8
OSGbear <mihail.tkach.nik@gmail.com>
2023-01-09 17:30:33 +0200

    добавлен цикл с исключением

---
+++ counter_with_exception/counter_with_exception.cpp
@@ -0,0 +1,25 @@
+﻿// Программа Finicky Counter
+// Демонстрирует работу с инструкциями break и continue
+#include <iostream>
+using namespace std;
+int main()
+{
+	int count = 0;
+	while (true)
+	{
+		count += 1;
+// заканчиваем цикл,если значение count превышает 10
+	if (count > 10)
+
+		{
+		break;
+		}
+	// пропускаем число 5
+	if (count == 5)
+		{
+		continue;
+		}
+	 cout << count << endl;
+	}
+return 0;
+}
