e4c583108724539dfa91148fdf6fed8f424409f8
OSGbear <mihail.tkach.nik@gmail.com>
2023-01-10 22:06:49 +0200

    Добавлена игра угадай число

---
+++ randG/randG.cpp
@@ -0,0 +1,18 @@
+﻿// Программа Die Roller
+// Демонстрирует генерирование случайных чисел
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+	while (true)
+	{
+		srand(static_cast<unsigned int>(time(0))); //зерно для псевдо рандома
+		// запускаем генератор случайных чисел
+		int randomNumber = rand(); // генерируем случайное число
+		int die = (randomNumber % 6) + 1; // получаем число между 1 и 6
+		cout << "You rolled a " << die << endl;
+	}
+	return 0;
+}
---
+++ randMyNumber/randMyNumber.cpp
@@ -0,0 +1,38 @@
+﻿
+// Игра Guess Му Number
+// Классическая игра в угадывание чисел
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+	int main()
+{
+		srand(static_cast<unsigned int>(time(0))); // запускаем генератор случайных чисел
+		int secretNumber = rand() % 100 + 1; //случайное число в диапазоне от 1до100
+		int tries = 0;
+		int guess;
+		cout << "\tWelcome to Guess My Number 1...100\n\n";
+				
+		//Соэдание игрового цикла
+			do
+	{
+				cout << "Enter а guess: ";
+				cin >> guess;
+				++tries;
+			if (guess > secretNumber)
+			{
+				cout << "Too high!\n\n";
+			}
+			else if (guess < secretNumber)
+			{
+				cout << "Too low!\n\n";
+			}
+			else
+			{
+				cout << "\nThat's it! You got it in\t" << tries << " guesses!\n";
+			}
+	} 
+			while (guess != secretNumber);
+	return 0;
+}
