f6d2a665ac7b93a3b4bef8bb5adecd93eee8450f
OSGbear <mihail.tkach.nik@gmail.com>
2023-01-11 14:24:18 +0200

    цикл в цикле

---
+++ forX/forX.cpp
@@ -0,0 +1,20 @@
+﻿//я испытал фантомную боль мозга пока разбирался как это работает
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "\n\nCounting with nested for loops:\n";
+	const int ROWS = 5;
+	const int COLUMNS = 3;
+	for (int i = 0; i < ROWS; ++i)
+	{
+		for (int j = 0; j < COLUMNS; ++j)
+	{
+			std::cout << i << "." << j << " ";
+	}
+		std::cout << std::endl;
+
+	}
+	return 0;
+}
