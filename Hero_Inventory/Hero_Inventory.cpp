441d0534c60d93acb19d5064ba88580b6d1d6339
OSGbear <mihail.tkach.nik@gmail.com>
2023-01-12 23:13:00 +0200

    крестики нолики (прототип), инвентарь, двумерный масив, 3х3

---
+++ Hero_Inventory/Hero_Inventory.cpp
@@ -0,0 +1,47 @@
+﻿// Hero_Inventory.cpp :
+
+// Программа Hero's Inventory
+// Демонстрирует работу с массивами
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	const int MAX_ITEMS = 10;
+	string inventory[MAX_ITEMS];
+	int numltems = 0;
+	inventory[numltems++]="sword";
+	inventory[numltems++]="armor";
+	inventory[numltems++]="shield";
+	cout << "Your items:\n";
+	for (int i = 0; i < numltems; ++i)
+	{
+		cout << inventory[i] << endl;
+	}
+	cout << "\nYou trade your sword for a battle axe. ";
+	inventory[0] = "battl axe";
+	cout << "\nYour items:\n";
+	for (int i = 0; i < numltems; ++i)
+	{
+		cout << inventory[i] << endl;
+	}
+		cout << "\n The item name " << inventory[0] << " has ";
+		cout << inventory[0].size() << " letters in it. \n"; //.size() -функция-член; inventory[0]-элемент
+		cout << "\nYou find а healing potion.";
+	
+		if (numltems < MAX_ITEMS)
+		{
+		inventory[numltems++] = "healing potion";
+		}
+		else
+		{
+		cout << "You have too many items and can't carry another.";
+		}
+		cout << "\nYour items:\n";
+	
+		for (int i = 0; i < numltems; ++i)
+		{
+			cout << inventory[i] << endl;
+		}
+			return 0;
+	}
---
+++ XOXO/XOXO.cpp
@@ -0,0 +1,35 @@
+﻿// XOXO.cpp
+
+// Демонстрирует работу с многомерными массивами
+#include <iostream>
+using namespace std;
+int main()
+{
+	const int ROWS = 3;
+	const int COLUMNS = 3;
+	char board[ROWS][COLUMNS] = { {'o','x','o'},
+	{' ', 'x' , 'x'},
+	{'x', 'o', 'o'} };
+	cout << "Here's the tic-tac-toe board:\n";
+	for (int i = 0; i < ROWS; ++i)
+	{
+		for (int j = 0; j < COLUMNS; ++j)
+		{
+			cout << board[i][j];
+		}
+		cout << endl;
+	}
+	cout << "\n'x' moves to the empty location.\n\n";
+	board[1][0] = 'x';
+	cout << "Now the tic-tac-toe board is:\n";
+	for (int i = 0; i < ROWS; ++i)
+	{
+		for (int j = 0; j < COLUMNS; ++j)
+		{
+			cout << board[i][j];
+		}
+		cout << endl;
+	}
+		cout << "\n'x' wins!";
+		return 0;
+	}
