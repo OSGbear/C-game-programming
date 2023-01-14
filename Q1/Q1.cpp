244f64562e49cf57847ced413681079288ed9594
OSGbear <mihail.tkach.nik@gmail.com>
2023-01-15 01:56:56 +0200

    Закончил словомеску

--- Q1/Q1.cpp
+++ Q1/Q1.cpp
@@ -10,11 +10,10 @@ int main()
 	const int NUM_WORDS = 5;
 	const string WORDS[NUM_WORDS][NUM_FIELDS] =
 	{
-
-	{"wall", "Do you feel you're banging your head against something?"},
-	{"glasses", "These might help you see the answer. "},
-	{"labored", "Going slowly. is it?"},
-	{"persistent", "Кеер at it."}, {"jumЫe", "It's what the game is all about."}
+		{"wall", "Do you feel you're banging your head against something?"},
+		{"glasses", "These might help you see the answer. "},
+		{"labored", "Going slowly. is it?"},
+		{"persistent", "Кеер at it."}, {"jumЫe", "It's what the game is all about."}
 	};
 	srand(static_cast<unsigned int>(time(0)));
 	int choice = (rand() % NUM_WORDS);
@@ -30,3 +29,33 @@ int main()
 		char temp = jumble[index1];
 		jumble[index1] = jumble[index2];
 		jumble[index2] = temp;
+	}
+	cout << "\t\t\tWelcome to Word JumЬle!\n\n";
+	cout << "Unscramble е the letters to make а word. \n";
+	cout << "Enter 'hint' for а hint. \n";
+	cout << "Enter 'quit' to quit the game. \n\n";
+	cout << "The jumble is: " << jumble;
+	string guess;
+	cout << "\n\nYour guess: ";
+	cin >> guess;
+
+	while ((guess != theWord)&&(guess != "quit"))
+	{
+		if (guess == "hint")
+		{
+			cout << theHint;
+		}
+		else
+		{
+			cout << "Sorry. that's not it. ";
+		}
+		cout << "\n\nYour guess: ";
+		cin >> guess;
+	}
+	if (guess == theWord)
+	{
+		cout << "\nThat's it! You guessed it!\n";
+	}
+	cout << "\nThanks for playing.\n";
+	return 0;
+}
