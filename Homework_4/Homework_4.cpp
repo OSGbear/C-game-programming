2f279561f73c649edde4aa2473e20d05dcb4cbaa
OSGbear <mihail.tkach.nik@gmail.com>
2023-01-27 19:33:13 +0200

    возвращаемые значения

--- Homework_4/Homework_4.cpp
+++ Homework_4/Homework_4.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main()
 {
-	vector<string> myGames;
+	vector <string> myGames;
 	string command;
 	int position;
 
---
+++ YN/YN.cpp
@@ -0,0 +1,36 @@
+﻿// Программа Yes or No
+// Демонстрирует работу с возвращаемыми значениями и параметрами
+#include <iostream>
+#include <string>
+using namespace std;
+char askYesNol();
+	char askYesNo2(string question);
+int main()
+{
+	char answerl = askYesNol();
+	cout << "Thanks for answering: " << answerl << "\n\n";
+	char answer2 = askYesNo2("Do you wish to save your game?");
+	cout << "Thanks for answering: " << answer2 << "\n";
+	return 0;
+}
+char askYesNol()
+{
+	char response1;
+	do
+	{
+		cout << "Please enter 'y' or 'n': ";
+		cin >> response1;
+	} while (response1 != 'y' && response1 != 'n');
+	return response1;
+}
+char askYesNo2(string question)
+{
+	char response2;
+		do
+		{
+			cout << question << " (y/n): ";
+			cin >> response2;
+		} 
+	while (response2 != 'y' && response2 != 'n');
+		return response2;
+}
