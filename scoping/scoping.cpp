2eeff3029156dceab843505ff9a27bde8ab635bf
OSGbear <mihail.tkach.nik@gmail.com>
2023-01-30 20:04:57 +0200

    область видимости (исправления)

--- scoping/scoping.cpp
+++ scoping/scoping.cpp
@@ -11,9 +11,11 @@ void func();
 		cout <<"Back in main() var is: " << var << "\n\n";
 		cout << "In main() in а new scope var is: " << var << "\n\n";
 		cout << "Creating new var in new scope.\n";
-		int var = 10; // переменная в новой области видимости
-					  // скрывает другую переменную.называемую var
-		cout << "In main() in а new scope var is: " << var << "\n\n";
+		{
+			int var = 10; // переменная в новой области видимости
+						  // скрывает другую переменную.называемую var
+			cout << "In main() in а new scope var is: " << var << "\n\n";
+		}
 		cout << "At end of main() var created in new scope no longer exists.\n";
 		cout << "At end of main() var is: " << var << "\n";
 	return 0;
