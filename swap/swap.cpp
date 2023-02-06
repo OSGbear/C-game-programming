581e0832b499e2e1676e27f19a4e7abc695f5e1e
OSGbear <mihail.tkach.nik@gmail.com>
2023-02-06 18:47:19 +0200

    пример работы со ссылками

---
+++ swap/swap.cpp
@@ -0,0 +1,37 @@
+﻿// Программа Swap
+// Демонстрирует передачу ссылок для изменения переменных - аргументов
+#include <iostream>
+using namespace std;
+void badSwap(int х, int у);
+void goodSwap(int& х, int& у) ;
+int main()
+{
+	int myScore = 150;
+	int yourScore = 1000;
+	cout << "Original values\n";
+	cout << "myScore; " << myScore << "\n";
+	cout << "yourScore; " << yourScore << "\n\n";
+	cout << "Calling badSwap()\n";
+
+	badSwap(myScore, yourScore);
+	cout << "myScore; " << myScore << "\n";
+	cout << "yourScore; " << yourScore << "\n\n";
+
+	cout << "Calling goodSwap()\n";
+	goodSwap(myScore, yourScore);
+	cout << "myScore; " << myScore << "\n";
+	cout << "yourScore; " << yourScore << "\n";
+	return 0;
+}
+void badSwap(int х, int у)
+{
+	int temp = х;
+	х = у;
+	у = temp;
+}
+void goodSwap(int& х, int& у)
+{
+	int temp = х;
+	х = у;
+	у = temp;
+}
