ccd10a208ecde43dfb46378430e50ccd7193fd1d
OSGbear <mihail.tkach.nik@gmail.com>
2023-02-18 22:18:47 +0200

    черновик

---
+++ CA/CA.cpp
@@ -0,0 +1,18 @@
+﻿// CA.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int& plusThree(int number);
+int main()
+{
+	std::cout<<plusThree(3);
+
+}
+
+int& plusThree(int number)
+{
+	
+	int threeMore = number + 3;
+	return threeMore;
+}
